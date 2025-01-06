74a9f926c48c4608f1c3dbacc4efd43628eab38f
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2025-01-06 23:43:47 +0600

    clint side str

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import MainLayouts from "./components/layout/MainLayouts";
+
+function App() {
+  return (
+    <>
+      <MainLayouts></MainLayouts>
+    </>
+  );
+}
+
+export default App;
